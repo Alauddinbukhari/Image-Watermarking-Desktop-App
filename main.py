17a135e31b862a403e95a3f7d6b8d3fdfa6aed01
Syed Alauddin Bukhari <syedalauddin.b@gmail.com>
2024-02-14 16:34:06 -0700

    make changes in main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+#Create Basic Tkinter Gui
+
+from tkinter import  *
+
+window = Tk()
+window.minsize(width=500,height=500)
+
+#input=  a image
+
+#output=  a image with my water mark
+
+
+
+#Add a file locator and load that image in variable
+
+#try to find a way to insert the watermark in Image
+
+#after that try to find if u can  5
+
+mainloop()
