5f8dc1daf65bf2c84dfffd41f25d309ebc478eb5
Syed Alauddin Bukhari <syedalauddin.b@gmail.com>
2024-02-16 12:15:38 -0700

    added font folder and as well as make some changes in main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from tkinter import  *
 from tkinter import filedialog
 from PIL import  Image, ImageDraw, ImageFont
-
+from random import  randint
 OUTPUT_PATH=r".\static\done_watermarking\image_with_watermark.png"
 WATERMARK_TEXT="Official_Beast"
 window = Tk()
@@ -37,7 +37,7 @@ def insert_watermark(input_image_path,output_image_path,watermark_text,font_size
     watermark = Image.new("RGBA", original_image.size, (255, 255, 255, 0))
 
     # Choose a font and size for the watermark
-    font = ImageFont.truetype("path/to/your/font.ttf", font_size)
+    font = ImageFont.truetype("./font\LoveDays-2v7Oe.ttf", font_size)
 
     # Create a drawing context for the watermark
     draw = ImageDraw.Draw(watermark)
@@ -45,12 +45,15 @@ def insert_watermark(input_image_path,output_image_path,watermark_text,font_size
     # Calculate the size of the watermark text
     text_size = draw.textbbox((0, 0), watermark_text, font)
 
-    # Calculate the position to center the watermark on the image
-    x = (original_image.width - text_size[2]) // 2
-    y = (original_image.height - text_size[3]) // 2
+    for i in range(6):
+        width_plus = randint(0, 6)
+        height_plus = randint(0, 6)
+        # Calculate the position to center the watermark on the image
+        x = (original_image.width + width_plus - text_size[2]) // 2
+        y = (original_image.height + height_plus - text_size[3] ) // 2
 
-    # Draw the watermark text on the transparent image
-    draw.text((x, y), watermark_text, font=font, fill=(255, 255, 255, 128))
+        # Draw the watermark text on the transparent image
+        draw.text((x, y), watermark_text, font=font, fill=(255, 255, 255, 128))
 
     # Composite the original image and the watermark
     watermarked_image = Image.alpha_composite(original_image.convert("RGBA"), watermark)
